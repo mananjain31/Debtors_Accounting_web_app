738e60e3dcec347a6ef0ddb3756876ca494e2f85
Manan Jain <mananjain31jan@gmail.com>
2021-12-23 19:12:39 +0530

    :sparkles: add function of datalayer with tests for UnitOfMeasurement

---
+++ datalayer-testcases/TestUOM.js
@@ -0,0 +1,18 @@
+const entities = require('datalayer/entities.js');
+const managers = require('datalayer/managers.js');
+if(process.argv.length == 2) return console.log(`You need to pass operation and data`);
+const testWhat = process.argv[2];
+
+// add 
+if(testWhat == 'add')
+{
+    // check if data given
+    if(process.argv.length < 4) return console.log(`Data to add is missing`);
+    const name =  process.argv[3];
+    const unitOfMeasurement = new entities.UnitOfMeasurement(0, name);
+    const uom_manager = new managers.UnitOfMeasurementManager();
+    uom_manager
+    .add(unitOfMeasurement)
+    .then(()=> console.log("Succesfully added : ", unitOfMeasurement))
+    .catch(err=>console.log(err))
+} //add ends
